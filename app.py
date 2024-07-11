ad7faa0172de4257ee5a056aad2769d34bdf37d6
Himani Dua <112327821+himani18dua@users.noreply.github.com>
2024-07-12 00:14:14 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
 from flask import Flask
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app, origins=["https://frontend-react-wc.vercel.app"])  # Allow only specific origin
 
 @app.route("/")
 def hello_world():
     return "<p>Hello, World!</p>"
+
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
